698961ecf19dd427408d0ac184b18856c8bfaa81
Austin Bolton <aibolt3@gmail.com>
2023-09-01 11:04:34 -0500

    times up

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,7 +3,30 @@ public class BankAccount {
     double savingsBalance;
 
     private static int accountsCreated = 0;
-    private static int totalAmount = 0;
+    private static double totalAmount = 0.0;
 
+    public BankAccount(double initialCheckingBal, double initialSavingsBal){
+        this.checkingBalance = initialCheckingBal;
+        this.savingsBalance = initialSavingsBal;
+        accountsCreated++;
+        totalAmount += (initialCheckingBal + initialSavingsBal);
+    }
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public void deposit(double amount, String account){
+        if (account.equalsIgnoreCase("checking")) {
+            checkingBalance += amount;
+        } else if (account.equalsIgnoreCase("saving")){
+            savingsBalance += amount;
+        }
+        totalAmount += amount;
+    }
 
 }
